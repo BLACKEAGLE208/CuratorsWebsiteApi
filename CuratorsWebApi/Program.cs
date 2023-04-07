42999205af57168879b1f2e3a2b5ae4f9a41e51b
BLACKEAGLE208 <49914017+BLACKEAGLE208@users.noreply.github.com>
2023-04-07 17:39:53 +0300

    Added database

--- CuratorsWebApi/Program.cs
+++ CuratorsWebApi/Program.cs
@@ -1,9 +1,14 @@
+using CuratorsWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<CuratorSiteContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CuratorsSiteContext")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
